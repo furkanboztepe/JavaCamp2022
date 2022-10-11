d985ccba6e72028509bf5875dc854cf11dce3a22
furkanboztepe <furkanboztepe58@hotmail.com>
2022-10-11 21:30:25 +0300

    second commit.

---
+++ src/helloworld/Customer.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class Customer {
+int id = 3;
+String name = "John";
+int age = 23;
+}
--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		Customer customer = new Customer();
+		customer.age=25;
+		System.out.println(customer.age);
 
 	}
 
